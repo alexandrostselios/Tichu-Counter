b25a09b0da92f53807e8d4d82745971d698644b8
Alexandros Tselios <alexTselios@users.noreply.github.com>
2018-11-25 20:21:33 +0200

    - Minor Changes

--- app/src/main/java/com/example/alexa/tichucounter/TichuCounter.java
+++ app/src/main/java/com/example/alexa/tichucounter/TichuCounter.java
@@ -2,6 +2,8 @@ package com.example.alexa.tichucounter;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -60,13 +62,25 @@ public class TichuCounter {
             }
             checkWinner();
         }
+        setScoreTeam1(score1);
+        setScoreTeam2(score2);
+        Log.d(null, "currentScore1: "+getScoreTeam1());
+        Log.d(null, "currentScore2: "+getScoreTeam2());
     }
 
-    public int getScoreteam1(){
+    private void setScoreTeam1(int score1){
+        this.score1=score1;
+    }
+
+    private void setScoreTeam2(int score2){
+        this.score2=score2;
+    }
+
+    public int getScoreTeam1(){
         return score1;
     }
 
-    public int getScoreteam2(){
+    public int getScoreTeam2(){
         return score2;
     }
 
@@ -75,6 +89,7 @@ public class TichuCounter {
             Log.i("winner","+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             return true;
         }
+        Log.i("winner","------------------------------------------------------------------");
         return false;
     }
 
@@ -93,5 +108,43 @@ public class TichuCounter {
             }
             win = true;
         }
+    }
+
+    public void checkTichuStatus(int teamTichu1, CheckBox tichuCheck1, Button grandTichu1, int teamGrandTichu1, CheckBox grandTichuCheck1, int teamTichu2, CheckBox tichuCheck2, Button grandTichu2, int teamGrandTichu2, CheckBox grandTichuCheck2){
+        if((teamTichu1==1 && teamGrandTichu1==1 && tichuCheck1.isChecked() && grandTichuCheck1.isChecked())
+                || (teamTichu1==1 && teamTichu2==1 && tichuCheck1.isChecked() && tichuCheck2.isChecked())
+                || (teamTichu1==1 && teamGrandTichu2==1 && tichuCheck1.isChecked() && grandTichuCheck2.isChecked())
+                || (teamTichu2==1 && teamGrandTichu2==1 && tichuCheck2.isChecked() && grandTichuCheck2.isChecked())
+                || (teamGrandTichu1==1 && teamTichu2==1 && grandTichuCheck1.isChecked() && tichuCheck2.isChecked())
+                || (teamGrandTichu1==1 && teamGrandTichu2==1 && grandTichuCheck1.isChecked() && grandTichuCheck2.isChecked())){
+
+            Toast.makeText(context.getApplicationContext(), "Check Tichu/Grand", Toast.LENGTH_SHORT).show();
+            error=1;
+            return;
+        }
+        if(teamTichu1==1 && tichuCheck1.isChecked()){
+            Log.d("null", "Team 1 TICHU");
+            score1 += 100;
+        }else if(teamTichu1==1 && !tichuCheck1.isChecked()){
+            score1 -= 100;
+        }
+        if(teamGrandTichu1==1 && grandTichuCheck1.isChecked()){
+            Log.d("null", "Team 1 GRAND TICHU");
+            score1 += 200;
+        }else if(teamGrandTichu1==1 && !grandTichuCheck1.isChecked()){
+            score1 -= 200;
+        }
+        if(teamTichu2==1 && tichuCheck2.isChecked()){
+            score2 += 100;
+        }else if(teamTichu2==1 && !tichuCheck2.isChecked()){
+            score2 -= 100;
+        }
+        if(teamGrandTichu2==1 && grandTichuCheck2.isChecked()){
+            score2 += 200;
+        }else if(teamGrandTichu2==1 && !grandTichuCheck2.isChecked()){
+            score2 -= 200;
+        }
+        /*Log.d("null", "score1: "+score1);
+        Log.d("null", "score2: "+score2);*/
     }
 }
