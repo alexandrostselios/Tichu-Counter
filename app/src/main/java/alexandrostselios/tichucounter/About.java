920c7477e66c0eac49c9fd4182882bd83ca33de0
Alexandros Tselios <9367330+alexandrostselios@users.noreply.github.com>
2021-05-10 17:37:18 +0300

    Add File Support

--- app/src/main/java/alexandrostselios/tichucounter/About.java
+++ app/src/main/java/alexandrostselios/tichucounter/About.java
@@ -1,13 +1,32 @@
 package alexandrostselios.tichucounter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        writeToFile("test",this);
         setContentView(R.layout.activity_menu_about);
+
+    }
+
+    private void writeToFile(String data, Context context) {
+        try {
+            Log.d(null, "inside try");
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("test.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
