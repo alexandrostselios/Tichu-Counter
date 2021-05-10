c5ece845ba23d11687e4390899d8b3d29b006815
Alexandros Tselios <9367330+alexandrostselios@users.noreply.github.com>
2021-05-10 17:46:39 +0300

    Add File Support and check if file exist

--- app/src/main/java/alexandrostselios/tichucounter/About.java
+++ app/src/main/java/alexandrostselios/tichucounter/About.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
@@ -21,9 +24,15 @@ public class About extends AppCompatActivity {
     private void writeToFile(String data, Context context) {
         try {
             Log.d(null, "inside try");
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("test.txt", Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            outputStreamWriter.close();
+            File file = new File("/data/data/alexandrostselios.tichucounter/files/test.txt");
+            if (file.exists()) {
+                Log.d(null, "EXIST");
+            }else{
+                Log.d(null, "does not EXIST");
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("test.txt", Context.MODE_PRIVATE));
+                outputStreamWriter.write(data);
+                outputStreamWriter.close();
+            }
         }
         catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
