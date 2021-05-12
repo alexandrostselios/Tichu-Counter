2714faf3b5e1530e97ee60194d1e3fac2e25e695
Alexandros Tselios <alexandrostsel@hotmail.com>
2021-05-12 22:21:50 +0300

    minor changes

--- app/src/main/java/alexandrostselios/tichucounter/Save.java
+++ app/src/main/java/alexandrostselios/tichucounter/Save.java
@@ -18,6 +18,7 @@ public class Save extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_menu_save);
+        
 
         // Create a FileManager object to use files
         try {
