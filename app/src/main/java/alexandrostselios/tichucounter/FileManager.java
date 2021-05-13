8b9f66a34e651cdf3511b662b3e1e6ee5245686f
Alexandros Tselios <alexandrostsel@hotmail.com>
2021-05-13 21:57:39 +0300

    - Write to file support was added - Write to Internal/Documents

--- app/src/main/java/alexandrostselios/tichucounter/FileManager.java
+++ app/src/main/java/alexandrostselios/tichucounter/FileManager.java
@@ -1,30 +1,42 @@
 package alexandrostselios.tichucounter;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.os.Environment.DIRECTORY_DOCUMENTS;
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
+import static android.support.v4.app.ActivityCompat.requestPermissions;
 
-public class FileManager {
+public class FileManager extends Activity {
 
     private Intent intent;
     private Context context;
     private File file;
+    private FileOutputStream fileOutputStream;
     private OutputStreamWriter outputStreamWriter;
-    private Date currentDate;
+    private String currentDate;
     private String[] writeData = new String[200];
 
     public FileManager(Intent intent, Save context) throws IOException {
@@ -32,9 +44,10 @@ public class FileManager {
         this.context=context;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void saveDataToFile() throws IOException {
         openFile();
-        currentDate = Calendar.getInstance().getTime();
+        currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
         writeData = intent.getStringArrayExtra("score");
         writeDataToFile();
     }
@@ -43,22 +56,27 @@ public class FileManager {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void openFile() throws IOException {
         //https://stackoverflow.com/questions/21216943/how-to-access-getfilesdir-as-an-environment-variable
         //https://gist.github.com/lopspower/76421751b21594c69eb2
-        file = new File( String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)),"/tichu.txt");
+        file = new File( String.valueOf(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS)),"/score.txt");
+        file.createNewFile();
         Log.d(null,file.getAbsolutePath());
-        outputStreamWriter = new OutputStreamWriter(context.openFileOutput(file.getName().toString(), Context.MODE_APPEND));
+        fileOutputStream = new FileOutputStream(file);
+        outputStreamWriter = new OutputStreamWriter(fileOutputStream);
     }
 
+
     private void writeDataToFile() throws IOException {
+        outputStreamWriter.write(currentDate+"\n");
         for(int i=0;i<writeData.length;i=i+2){
             if(writeData[i]!=null){
-                outputStreamWriter.write(currentDate +" "+writeData[i]+" "+writeData[i+1]+"\n");
+                outputStreamWriter.write(i+1 +") "+writeData[i]+" "+writeData[i+1]+"\n");
+                //Log.d(null,writeData[i]+" "+writeData[i+1]+"\n");
             }
         }
         outputStreamWriter.write(10);
-        outputStreamWriter.flush();
         outputStreamWriter.close();
     }
 }
