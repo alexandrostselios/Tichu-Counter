8afa46b511a8e270d6ebf3ed2a4b8bef37a50cb2
Alexandros Tselios <alexandrostsel@hotmail.com>
2021-06-09 22:20:34 +0300

    - Online database functions enabled.

--- app/src/main/java/alexandrostselios/tichucounter/DataBaseManager.java
+++ app/src/main/java/alexandrostselios/tichucounter/DataBaseManager.java
@@ -26,12 +26,15 @@ public class DataBaseManager extends Activity {
     private static Context context;
     private String[] writeData = new String[200];
     private static SQLiteDatabase mydatabase;
+    private final String server_url = "http://alefhome.ddns.net:2374/insert.php";
+    private RequestQueue queue = null;
 
     public DataBaseManager (SQLiteDatabase mydatabase,Context context){
         this.mydatabase=mydatabase;
         this.context = context;
-        String server_url = "http://alefhome.ddns.net:2374/insert.php";
-        RequestQueue queue;
+    }
+
+    private void writeToOnlineDatabase(String TeamID, int score1, int score2){
         queue = Volley.newRequestQueue(this.context);
         StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
             @Override
@@ -42,7 +45,8 @@ public class DataBaseManager extends Activity {
                     String message = jsonObject.getString("message");
                     if(success.equals("1")){
                         Log.d(null,"++++++++++++++++++++++++++++++");
-                        Log.d(null,"Response: : "+success + "\n Message: "+message);
+                        Log.d(null,"Response: : " + success + "\n");
+                        Log.d(null,"Message: " + message + "\n");
                         Log.d(null,"++++++++++++++++++++++++++++++");
                     }
                 } catch (JSONException e) {
@@ -60,8 +64,10 @@ public class DataBaseManager extends Activity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String,String> params = new HashMap<>();
-                params.put("Team1",GUI.TextScore1.getText().toString());
-                params.put("Team2",GUI.TextScore2.getText().toString());
+                params.put("Table","FinalScore");
+                params.put("TeamID", TeamID);
+                params.put("Team1",String.valueOf(score1));
+                params.put("Team2",String.valueOf(score2));
                 return params;
             }
         };
@@ -81,6 +87,7 @@ public class DataBaseManager extends Activity {
         resultSet.moveToFirst();
         index = resultSet.getString(0);
         mydatabase.execSQL("INSERT INTO ScoreHistory(TeamID,Score1,Score2) VALUES("+Integer.parseInt(index)+","+score1+","+score2+");");
+        writeToOnlineDatabase(index,score1,score2);
     }
 
     public static void revertScore(){
